16475480c519329b7e74236c7193f93a0da4515c
nakumamit27-beep <nakumamit27@gmail.com>
2026-09-16 05:53:29 +0000

    Convert TWA to Pure Native WebView

---
+++ app/src/main/java/com/wildsphere/app/MainActivity.java
@@ -0,0 +1,77 @@
+package com.wildsphere.app;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ValueCallback<Uri[]> uploadMessage;
+    private static final int FILECHOOSER_RESULTCODE = 1;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("file://") || url.contains("localhost")) return false;
+                if (url.startsWith("http://") || url.startsWith("https://")) return false;
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                } catch (Exception ignored) {}
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView wv, ValueCallback<Uri[]> cb, FileChooserParams fp) {
+                if (uploadMessage != null) uploadMessage.onReceiveValue(null);
+                uploadMessage = cb;
+                try {
+                    startActivityForResult(fp.createIntent(), FILECHOOSER_RESULTCODE);
+                } catch (Exception e) {
+                    uploadMessage = null;
+                    return false;
+                }
+                return true;
+            }
+        });
+
+        webView.loadUrl("file:///android_asset/public/index.html");
+    }
+
+    @Override
+    protected void onActivityResult(int req, int res, Intent data) {
+        super.onActivityResult(req, res, data);
+        if (req == FILECHOOSER_RESULTCODE && uploadMessage != null) {
+            uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(res, data));
+            uploadMessage = null;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
